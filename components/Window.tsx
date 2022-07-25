9ea2ffc8141fb364c8dec3038a26f3cc747b994f
Josh Wells <josh@zakini.co.uk>
2022-07-25 23:41:18 +0100

    feat: scrollbar thumbs are now draggable

--- components/Window.tsx
+++ components/Window.tsx
@@ -39,6 +39,7 @@ type ScrollBarThumbProps = {
   spaceShown: number | null
   totalSpace: number | null
   position: number
+  onDrag: (amount: number) => void
 }
 
 type ScrollBarButtonProps = HTMLAttributes<HTMLButtonElement> & {
@@ -46,9 +47,11 @@ type ScrollBarButtonProps = HTMLAttributes<HTMLButtonElement> & {
   active: boolean
 }
 
-type ScrollBarProps = Omit<HTMLAttributes<HTMLDivElement>, 'onScroll'> & ScrollBarThumbProps & {
-  onScroll: (axis: Axis, amount: number) => void
-}
+type ScrollBarProps = Omit<HTMLAttributes<HTMLDivElement>, 'onScroll'>
+  & Omit<ScrollBarThumbProps, 'onDrag'>
+  & {
+    onScroll: (axis: Axis, amount: number) => void
+  }
 
 type FrameProps = PropsWithChildren<HTMLAttributes<HTMLDivElement>> & {
   collapsed: boolean
@@ -126,7 +129,7 @@ const Frame = ({ className, collapsed, children, ...props }: FrameProps) => (
   </div>
 )
 
-const ScrollBarThumb = ({ axis, spaceShown, totalSpace, position }: ScrollBarThumbProps) => {
+const ScrollBarThumb = ({ axis, spaceShown, totalSpace, position, onDrag }: ScrollBarThumbProps) => {
   const displayFraction = spaceShown === null || totalSpace === null ? null : spaceShown / totalSpace
   const thumbSpanDirection = axis === 'x' ? 'h-full' : 'w-full'
   const thumbFillDirection = axis === 'x' ? 'width' : 'height'
@@ -135,17 +138,19 @@ const ScrollBarThumb = ({ axis, spaceShown, totalSpace, position }: ScrollBarThu
   const thumbSize = displayFraction === null ? '0' : `calc(${displayFraction * 100}% + 2px)`
 
   return (
-    <div
-      className={`absolute flex place-content-center place-items-center top-0 bg-[rgb(153,153,255)] ${thumbSpanDirection} ${thumbBorderEnds} border-[rgb(28,28,40)]`}
-      style={{
-        [thumbFillDirection]: thumbSize,
-        // TODO bounds of thumb need to be 1px beyond ScrollBar limits to prevent double border
-        [thumbMoveDirection]: `calc((100% - ${thumbSize} + 2px) * ${position} - 1px)`,
-        boxShadow: `${axis === 'x' ? '1px 0' : '0 1px'} 2px rgb(106,106,106)`
-      }}
-    >
-      <Grill className={`h-[8px] w-[8px] ${axis === 'x' ? '-rotate-90' : ''}`} colourA='rgb(166,166,229)' colourB='rgb(89,89,179)' />
-    </div>
+    <Draggable onDrag={(e, { [axis === 'x' ? 'deltaX' : 'deltaY']: amount }) => onDrag(amount)}>
+      <div
+        className={`absolute flex place-content-center place-items-center top-0 bg-[rgb(153,153,255)] ${thumbSpanDirection} ${thumbBorderEnds} border-[rgb(28,28,40)]`}
+        style={{
+          [thumbFillDirection]: thumbSize,
+          // TODO bounds of thumb need to be 1px beyond ScrollBar limits to prevent double border
+          [thumbMoveDirection]: `calc((100% - ${thumbSize} + 2px) * ${position} - 1px)`,
+          boxShadow: `${axis === 'x' ? '1px 0' : '0 1px'} 2px rgb(106,106,106)`
+        }}
+      >
+        <Grill className={`h-[8px] w-[8px] ${axis === 'x' ? '-rotate-90' : ''}`} colourA='rgb(166,166,229)' colourB='rgb(89,89,179)' />
+      </div>
+    </Draggable>
   )
 }
 
@@ -191,12 +196,17 @@ const ScrollBar = ({ axis, spaceShown, totalSpace, position, onScroll, className
             className={`h-full ${scrollable ? 'bg-[rgb(170,170,170)]' : 'bg-[rgb(238,238,238)]'}`}
             style={scrollable ? { boxShadow: 'inset 1px 1px 2px rgb(106,106,106)' } : {}}
           />
-          {/* Thumb */}
           {scrollable
-            ? <ScrollBarThumb position={thumbPosition} {...({ axis, spaceShown, totalSpace })} />
+            ? <ScrollBarThumb
+              position={thumbPosition}
+              // HACK need to boost the scroll speed here to match how the mouse moves, why?
+              onDrag={amount => onScroll(axis, amount * (totalSpace / spaceShown) * 1.21)}
+              {...({ axis, spaceShown, totalSpace })}
+            />
             : null}
         </div>
         <div className={`z-10 flex-none flex ${flexDirection} ${dividerSide} ${scrollable ? 'border-[rgb(53,53,53)]' : 'border-[rgb(123,123,123)]'}`}>
+          {/* TODO click and hold to keep scrolling */}
           <ScrollBarButton
             direction={axis === 'x' ? 'left' : 'up'}
             active={scrollable}
